ae8a90681db0c0081561ee21f85a631a8176d72e
Auska <luodan0709@live.cn>
2026-01-01 15:12:15 +0800

    remove build

--- build/CMakeFiles/etracker.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for etracker.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/etracker.dir/src/math.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/etracker.dir/src/math.c.o: /home/code/etracker/src/math.c \
- /usr/include/stdc-predef.h /home/code/etracker/include/et_math.h
